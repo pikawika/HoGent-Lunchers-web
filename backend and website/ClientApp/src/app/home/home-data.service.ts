dc15629b115947ffdf3cf40e8c2c4e0ec7d9b0d4
jodidl1998 <jodi.deloof.y0180@student.hogent.be>
2018-11-02 16:51:20 +0100

    basic rest api connection

---
+++ backend and website/ClientApp/src/app/home/home-data.service.ts
@@ -0,0 +1,26 @@
+import {map} from 'rxjs/operators';
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {Lunch} from '../../models/lunch';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeDataService {
+
+  private _baseUrl:string; 
+
+  constructor(private http:HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this._baseUrl = baseUrl; 
+  }
+
+  get lunches():Observable<Lunch[]>{
+    
+    return this.http.get(this._baseUrl+"api/Lunch/getAll").pipe(
+      map((list: any[]): Lunch[]=>
+        list.map(Lunch.fromJSON)
+      )
+    );
+  }
+}
--- backend and website/ClientApp/src/app/home/home.component.ts
+++ backend and website/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HomeDataService } from './home-data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+
+  public _lunches; //public voor html
+  public _baseUrl;
+
+  constructor(public dataService: HomeDataService,@Inject('BASE_URL') baseUrl: string){
+    this._baseUrl = baseUrl;
+  }
+
+  ngOnInit(){
+    this.dataService.lunches.subscribe(lunches => {
+      this._lunches = lunches;
+    });
+  }
 }
