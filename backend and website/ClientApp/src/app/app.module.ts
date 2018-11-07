7dd226fe6e1bc1828c4b1fcc635c8ec197784ca9
jodidl1998 <jodi.deloof.y0180@student.hogent.be>
2018-11-07 22:19:33 +0100

    login + register and authentication guards

--- backend and website/ClientApp/src/app/app.module.ts
+++ backend and website/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -9,6 +9,12 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { LunchDetailComponent } from './lunch-detail/lunch-detail.component';
 import { LunchComponent } from './lunch/lunch.component';
+import { RegisterComponent } from './user/register/register.component';
+import { httpInterceptorProviders } from './http-interceptors';
+import { LoginComponent } from './user/login/login.component';
+import { LogoutComponent } from './user/logout/logout.component';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthGuardService } from './user/auth-guard.service';
 
 
 @NgModule({
@@ -17,19 +23,26 @@ import { LunchComponent } from './lunch/lunch.component';
     NavMenuComponent,
     HomeComponent,
     LunchDetailComponent,
-    LunchComponent
+    LunchComponent,
+    RegisterComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'details/:id', component:LunchDetailComponent}
+      { path: 'details/:id',canActivate: [ AuthGuardService ], component:LunchDetailComponent},
+      { path: 'register', component:RegisterComponent},
+      { path: 'login', component:LoginComponent},
+      { path:'logout', component:LogoutComponent}
     ]),
     
   ],
-  providers: [],
+  providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
