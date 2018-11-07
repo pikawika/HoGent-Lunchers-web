e651f05760151a05a15b18dc4b453d0818a4ae48
Lennert Bontinck <lennertbontinck@live.be>
2018-11-08 00:04:47 +0100

    gebruikerlogin gemapt

--- backend and website/Models/Domain/Gebruiker.cs
+++ backend and website/Models/Domain/Gebruiker.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace Lunchers.Models.Domain
         public int GebruikerId { get; private set; }
         public string Telefoonnummer { get; set; }
         public string Email { get; set; }
+        [JsonIgnore]
         public Login Login { get; set; } = new Login();
         public string Voornaam { get; set; }
         public string Achternaam { get; set; }
