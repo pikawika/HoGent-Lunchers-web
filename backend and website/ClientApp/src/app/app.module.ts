fe72b16afd6c67c31b542b755440a40603f3d0b8
Brent Schets <brent.schets.y9143@student.hogent.be>
2018-11-18 17:09:25 +0100

    specific lunches of a merchant

--- backend and website/ClientApp/src/app/app.module.ts
+++ backend and website/ClientApp/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 import { AuthGuardService } from './user/auth-guard.service';
 import { RegisterMerchantComponent } from './user/register-merchant/register-merchant.component';
 import { LunchMerchantComponent } from './merchant/lunch-merchant/lunch-merchant.component';
+import { AddLunchComponent } from './merchant/add-lunch/add-lunch.component';
 
 
 @NgModule({
@@ -30,7 +31,8 @@ import { LunchMerchantComponent } from './merchant/lunch-merchant/lunch-merchant
     LoginComponent,
     LogoutComponent,
     RegisterMerchantComponent,
-    LunchMerchantComponent
+    LunchMerchantComponent,
+    AddLunchComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -45,6 +47,7 @@ import { LunchMerchantComponent } from './merchant/lunch-merchant/lunch-merchant
       { path: 'logout', component:LogoutComponent},
       { path: 'merchantregister', component:RegisterMerchantComponent},
       { path: 'merchant/lunch', component:LunchMerchantComponent},
+      {path: 'merchant/addlunch', component:AddLunchComponent}
     ]),
     
   ],
---
+++ backend and website/ClientApp/src/app/merchant/add-lunch/add-lunch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-lunch',
+  templateUrl: './add-lunch.component.html',
+  styleUrls: ['./add-lunch.component.css']
+})
+export class AddLunchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
