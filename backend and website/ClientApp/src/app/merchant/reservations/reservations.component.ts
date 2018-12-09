3b55ba3df386c84a61bd00fda68a7831a76cdd2b
Brent Schets <brent.schets.y9143@student.hogent.be>
2018-12-09 23:12:03 +0100

    started working on approving and declining reservations

--- backend and website/ClientApp/src/app/merchant/reservations/reservations.component.ts
+++ backend and website/ClientApp/src/app/merchant/reservations/reservations.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MerchantDataService } from '../merchant-data.service';
 import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Reservatie } from 'src/models/reservatie';
 
 @Component({
   selector: 'app-reservations',
@@ -9,8 +11,11 @@ import { Router } from '@angular/router';
 })
 export class ReservationsComponent implements OnInit {
 
+  public errorMsg;
   public _baseUrl;
   public _reservaties;
+  public _goedTeKeuren = [];
+  public _goedGekeurd = [];
 
   constructor(
     @Inject('BASE_URL') baseUrl: string,
@@ -23,11 +28,46 @@ export class ReservationsComponent implements OnInit {
   ngOnInit() {
     this.dataService.reservations.subscribe(reservaties => {
       this._reservaties = reservaties;
+
+      this._reservaties.forEach(res => {
+        if(res.status == "In afwachting"){
+          this._goedTeKeuren.push(res);
+        }
+        if(res.status == "Goedgekeurd"){
+          this._goedGekeurd.push(res);
+        }
+      });
     });
   }
 
+  approved(reservatie: Reservatie){
+      this.dataService.approveReservation(reservatie.reservatieId).subscribe(
+        val => {
+          if (val) {
+            this.errorMsg = val;
+          }
+        },
+        (error: HttpErrorResponse) => {
+          this.errorMsg = error.error.error;     
+        }
+      );
+  }
+
+  declined(reservatieId){
+  }
+
+
+
   get reservations() {
     return this._reservaties;
+  }
+
+  get approvedReservations(){
+    return this._goedGekeurd;
+  }
+
+  get waitingReservations(){
+    return this._goedTeKeuren;
   }
 
 }
