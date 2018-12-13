e8fa9fc014827927f336effdd3f771cae7e2547e
jodidl1998 <jodi.deloof.y0180@student.hogent.be>
2018-12-13 22:32:25 +0100

    allergy base

---
+++ backend and website/Controllers/AllergyController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Lunchers.Models;
+using Lunchers.Models.Domain;
+using Lunchers.Models.GebruikerViewModels.GebruikerTaken;
+using Lunchers.Models.IRepositories;
+using Lunchers.Models.Repositories;
+using Lunchers.Models.ViewModels.Reservatie;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace Lunchers.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class AllergyController : ControllerBase
+    {
+        private IReservatieRepository _reservatieRepository;
+        private IKlantRepository _klantRepository;
+        private ILunchRespository _lunchRespository;
+        private IHandelaarRepository _handelaarRepository;
+
+        public AllergyController(IReservatieRepository reservatieRepository, IKlantRepository klantRepository, ILunchRespository lunchRespository, IHandelaarRepository handelaarRepository)
+        {
+            _reservatieRepository = reservatieRepository;
+            _klantRepository = klantRepository;
+            _lunchRespository = lunchRespository;
+            _handelaarRepository = handelaarRepository;
+        }
+
+        // GET: api/Reservatie
+        [HttpGet]
+        public IActionResult Get()
+        {
+            if (User.FindFirst("gebruikersId")?.Value != null && User.FindFirst("rol")?.Value == "klant")
+            {
+                Klant klant = _klantRepository.GetById(int.Parse(User.FindFirst("gebruikersId")?.Value));
+                return Ok(klant.Reservaties);
+            }
+            else if (User.FindFirst("gebruikersId")?.Value != null && User.FindFirst("rol")?.Value == "handelaar")
+            {
+                return Unauthorized(new { error = "U bent niet aangemeld als gebruiker." });
+            }
+            else if (User.FindFirst("gebruikersId")?.Value != null && User.FindFirst("rol")?.Value == "admin")
+            {
+                return Unauthorized(new { error = "U bent niet aangemeld als gebruiker." });
+            }
+            return Unauthorized(new { error = "U bent niet aangemeld." });
+        }
+
+        // POST: api/Reservatie
+        [HttpPost]
+        public IActionResult Post([FromBody]AllergieToevoegenViewModel nieuweAllergie)
+        {
+            if (User.FindFirst("gebruikersId")?.Value != null && User.FindFirst("rol")?.Value == "klant")
+            {
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        _klantRepository.AddAllergy(int.Parse(User.FindFirst("gebruikersId")?.Value),nieuweAllergie.Allergie);
+                    }
+                    catch
+                    {
+                        return BadRequest(new { error = "Er is iets fout gegaan tijdens het aanmaken van de reservatie." });
+                    }
+                }
+                return BadRequest(new { error = ModelState });
+            }
+            return Unauthorized(new { error = "U bent niet aangemeld als klant." });
+        }
+    }
+}
---
+++ backend and website/Models/ViewModels/Gebruiker/GebruikerTaken/AllergieToevoegenViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lunchers.Models.GebruikerViewModels.GebruikerTaken
+{
+    public class AllergieToevoegenViewModel
+    {
+        [Required(ErrorMessage = "{0} is verplicht.")]
+        [DataType(DataType.Text, ErrorMessage = "Geen geldig {0} ingevoerd")]
+        public string Allergie { get; set; }
+    }
+}
