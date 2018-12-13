8970e68f0730648f966433a523352b07648e71ae
Brent Schets <brent.schets.y9143@student.hogent.be>
2018-12-13 16:41:24 +0100

    accepet merchant and fixed little bug

--- backend and website/ClientApp/src/app/admin-panel/admin-merchants/admin-merchants.component.ts
+++ backend and website/ClientApp/src/app/admin-panel/admin-merchants/admin-merchants.component.ts
@@ -13,6 +13,8 @@ export class AdminMerchantsComponent implements OnInit {
 
   public _baseUrl;
   public _merchants;
+  public _approvedMerchants = [];
+  public _approvableMerchtans = [];
 
   constructor(
     @Inject('BASE_URL') baseUrl: string,
@@ -25,11 +27,26 @@ export class AdminMerchantsComponent implements OnInit {
   ngOnInit() {
     this.dataService.merchants.subscribe(merchants => {
       this._merchants = merchants;
+      this._merchants.forEach(merchant => {
+        if(merchant.login.geactiveerd){
+          this._approvedMerchants.push(merchant);
+        }else{
+          this._approvableMerchtans.push(merchant);
+        }
+      });
     });
   }
 
   get merchants() {
     return this._merchants;
+  }
+
+  get approvedMerchants(){
+    return this._approvedMerchants;
+  }
+
+  get approvableMerchants(){
+    return this._approvableMerchtans;
   }
 
   goToMerchantDetail(merchant: Handelaar) {
@@ -41,9 +58,14 @@ export class AdminMerchantsComponent implements OnInit {
     data.append("handelaarId", merchant.handelaarId);
     this.dataService.removeMerchant(data).subscribe(receivedData => {
       if (receivedData["status"] == 200) {
-        for (var i = this._merchants.length - 1; i >= 0; i--) {
-          if (this._merchants[i].handelaarId === merchant.handelaarId) {
-            this._merchants.splice(i, 1);
+        for (var i = this._approvableMerchtans.length - 1; i >= 0; i--) {
+          if (this._approvableMerchtans[i].handelaarId === merchant.handelaarId) {
+            this._approvableMerchtans.splice(i, 1);
+          }
+        }
+        for (var i = this._approvedMerchants.length - 1; i >= 0; i--) {
+          if (this._approvedMerchants[i].handelaarId === merchant.handelaarId) {
+            this._approvedMerchants.splice(i, 1);
           }
         }
       }
