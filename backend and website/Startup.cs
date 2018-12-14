f76aa5b8a1cc58ec655778cd29708522b11e8fb9
jodiDL <jodi@jodideloof.be>
2018-12-14 14:10:46 +0100

    remove double home

--- backend and website/Startup.cs
+++ backend and website/Startup.cs
@@ -34,7 +34,7 @@ namespace Lunchers
 
             //windows of mac invullen afhankelijk van je os -> Deployment voor azure -> server voor brent
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("server")));
+                options.UseSqlServer(Configuration.GetConnectionString("mac")));
 
             services.AddSpaStaticFiles(configuration =>
             {
