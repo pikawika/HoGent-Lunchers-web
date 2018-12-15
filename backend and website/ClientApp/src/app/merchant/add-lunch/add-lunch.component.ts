5b3aa0b9fe67a858422dfc6ba0b950e82ef308f0
Brent Schets <brent.schets.y9143@student.hogent.be>
2018-12-15 17:22:19 +0100

    some standart routing changes and button for adding ingredient

--- backend and website/ClientApp/src/app/merchant/add-lunch/add-lunch.component.ts
+++ backend and website/ClientApp/src/app/merchant/add-lunch/add-lunch.component.ts
@@ -73,6 +73,7 @@ export class AddLunchComponent implements OnInit {
     if (ingredient.length <= 20 && ingredient.length > 0) {
       ing.Naam = ingredient;
       this._ingredienten.push(ing);
+      (<HTMLInputElement>document.getElementById('ingredienten')).value = "";
     } else {
       this.errorMsg = "Er is een fout opgetreden bij het toevoegen van een ingredient(max 20 tekens)"
     }
